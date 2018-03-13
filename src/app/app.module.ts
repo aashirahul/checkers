6e94ffddc692165dc5a7490721418c9df62ede7d
Ashima Shrivastava <ashima.shrivastava@matrixres.com>
2018-03-13 15:28:51 -0400

    game-board up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { GameBoardComponent } from './components/game-board/game-board.component';
+import { CellComponent } from './components/cell/cell.component';
+import { PieceComponent } from './components/piece/piece.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+    CellComponent,
+    PieceComponent
   ],
   imports: [
     BrowserModule
